e646677060b4165d8b8129d77afdc86edca60b11
elzup <guild0105@gmail.com>
2020-02-13 23:41:08 +0900

    install styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,34 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
+import React from 'react'
 
-class Document extends NextDocument {
+class Document extends NextDocument<{}> {
+  static async getInitialProps(ctx) {
+    const sheet = new StyledComponentSheets()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await NextDocument.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
   render() {
     return (
       <Html lang={'ja'}>
