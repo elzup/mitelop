c9ed69edf89aaabd6ac8cfa6faf4225547bbfa4e
elzup <guild0105@gmail.com>
2020-02-13 10:28:12 +0900

    config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
-import Link from 'next/link'
 import { NextPage } from 'next'
 import Layout from '../components/Layout'
+
+type WindowOptions = {
+  url: string
+  name?: string
+  height?: number
+  width?: number
+}
+
+function windowOpen({ url, height, width, name = '_blank' }: WindowOptions) {
+  const option =
+    (height ? `height=${height}` : '') + (width ? `width=${width}` : '')
+
+  window.open(url, name, option)
+}
 
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1>Mitelop</h1>
       <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+        <a
+          onClick={() => {
+            window.open('/about', '_blank', 'width=200,height=200')
+          }}
+        >
+          About
+        </a>
       </p>
     </Layout>
   )
