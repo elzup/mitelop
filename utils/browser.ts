a57ff1852656f612f770c90537329f51e9d3ca89
elzup <guild0105@gmail.com>
2020-02-26 14:53:08 +0900

    refactor options

--- utils/browser.ts
+++ utils/browser.ts
@@ -8,10 +8,10 @@ export function windowOpen(
   url: string,
   { height, width, name = '_blank' }: WindowOptions
 ) {
-  const options: string[] = []
+  const options: (string | false)[] = [
+    height !== undefined && `height=${height}`,
+    width !== undefined && `width=${width}`,
+  ]
 
-  if (height) options.push(`height=${height}`)
-  if (width) options.push(`width=${width}`)
-
-  window.open(url, name, options.join(','))
+  window.open(url, name, options.filter(Boolean).join(','))
 }
