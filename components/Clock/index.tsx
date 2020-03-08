f97d35e99239ace801994daf51eb8ee618b82c1f
elzup <guild0105@gmail.com>
2020-03-08 13:36:59 +0900

    replace use-seconds

--- components/Clock/index.tsx
+++ components/Clock/index.tsx
@@ -1,50 +1,18 @@
 import styled from 'styled-components'
-import { useState, useEffect } from 'react'
+import { useSeconds } from 'use-seconds'
 
-const roundSeconds = (t: Date): [Date, number] => {
-  const d = +t % 1000
-  const nextMs = 1000 - t.getMilliseconds()
-
-  if (d < 500) {
-    return [new Date(+t - d), nextMs]
-  }
-  return [new Date(+t + (1000 - d)), nextMs + 1000]
-}
-
-const useTime = () => {
-  const [time, setTime] = useState<Date>(new Date())
-
-  useEffect(() => {
-    let handle: number | null = null
-
-    const secondsCycle = () => {
-      const t = new Date()
-      const [fixedTime, nextMs] = roundSeconds(t)
-
-      handle = setTimeout(secondsCycle, Math.max(nextMs - 1, 1))
-
-      setTime(fixedTime)
-    }
-
-    secondsCycle()
-    return () => {
-      if (handle) {
-        clearTimeout(handle)
-      }
-    }
-  }, [])
-
-  return time
-}
+const dateStr = (t: Date) =>
+  `${t.getFullYear()}-${t.getMonth() + 1}-${t.getDate()}`
+const timeStr = (t: Date) =>
+  `${t.getHours()}-${t.getMinutes()}-${t.getSeconds()}`
 
 function Clock() {
-  const time = useTime()
+  const [time] = useSeconds()
 
   return (
     <Style>
-      <div className="panel">{String(time.getSeconds())}</div>
-      <div className="footer">{+time}</div>
-      <div className="footer">{String(time)}</div>
+      <span>{dateStr(time)}</span>
+      <span>{timeStr(time)}</span>
     </Style>
   )
 }
