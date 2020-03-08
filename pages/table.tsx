1fcc39bd01b1d772668a648a1ee74ecc1af36dcd
elzup <guild0105@gmail.com>
2020-03-08 15:01:28 +0900

    Revert "use use-query-params"
    This reverts commit d6c3afcba74d0594208e810e11ba88ba5baebaf4.

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,13 +1,17 @@
 import { NextPage } from 'next'
-import { useQueryParam, NumberParam, StringParam } from 'use-query-params'
+import { useRouter } from 'next/router'
 import Layout from '../components/Layout'
 import Table from '../components/Table'
 
+const getOne = (v: string | string[] | undefined): string => {
+  if (!v) return ''
+  return typeof v === 'object' ? v[0] : v
+}
+
 const IndexPage: NextPage = () => {
-  const titlesParam = useQueryParam('title', StringParam)[0] || ''
-  const rowParam = useQueryParam('row', NumberParam)[0]
-  const titles = titlesParam?.split(',')
-  const row = !!rowParam
+  const { query } = useRouter()
+  const titles = getOne(query['titles']).split(',')
+  const row = !!getOne(query['row'])
 
   return (
     <Layout title="Table List" reset>
