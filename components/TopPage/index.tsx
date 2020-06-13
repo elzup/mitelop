a40d3dff2d67d021618b26e703c1b30303746d4a
elzup <guild0105@gmail.com>
2020-06-13 17:13:48 +0900

    change tab UI TopPage

--- components/TopPage/index.tsx
+++ components/TopPage/index.tsx
@@ -1,17 +1,31 @@
-import { Typography } from '@material-ui/core'
+import { Typography, Tab, Tabs } from '@material-ui/core'
+import { useState } from 'react'
 import ClockGenerator from '../Clock/ClockGenerator'
 import KatinkoGenerator from '../Katinko/KatinkoGenerator'
 import StopwatchGenerator from '../Stopwatch/StopwatchGenerator'
 import TableGenerator from '../Table/TableGenerator'
 
 const TopPage = () => {
+  const [selected, setSelected] = useState<number>(0)
+
   return (
     <div style={{ margin: '8px' }}>
       <Typography variant="h4">Mitelop</Typography>
-      <TableGenerator />
-      <KatinkoGenerator />
-      <ClockGenerator />
-      <StopwatchGenerator />
+      <Tabs
+        value={selected}
+        onChange={(e, i) => setSelected(i)}
+        aria-label="simple tabs example"
+      >
+        <Tab label="テーブル" />
+        <Tab label="カチンコ" />
+        <Tab label="クロック" />
+        <Tab label="ストップウォッチ" />
+      </Tabs>
+
+      {selected === 0 && <TableGenerator />}
+      {selected === 1 && <KatinkoGenerator />}
+      {selected === 2 && <ClockGenerator />}
+      {selected === 3 && <StopwatchGenerator />}
     </div>
   )
 }
