ef771e924768199518e78869a37933f151c23e78
elzup <guild0105@gmail.com>
2020-06-13 17:18:16 +0900

    set theme color

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,6 +1,13 @@
 import { createMuiTheme } from '@material-ui/core'
+import { orange } from '@material-ui/core/colors'
 
 export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2B0065',
+    },
+    secondary: orange,
+  },
   props: {
     MuiTextField: {
       variant: 'outlined',
