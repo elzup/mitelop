7e5b4a9712e161198fd824562a5acac283eee87e
elzup <guild0105@gmail.com>
2020-08-02 16:04:43 +0900

    style Timer numbers

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -5,26 +5,33 @@ import { useTimer } from './useTimer'
 
 const pad2 = (n: number) => `${n}`.padStart(2, '0')
 
-const timeToStr = (t: number, showUnder = false) => {
+const timeToStr = (t: number): [string, string] => {
   const SEC = 1000
   const MIN = 60 * SEC
   const HOU = 60 * MIN
   const h = Math.floor(t / HOU)
   const m = Math.floor((t % HOU) / MIN)
   const s = Math.floor((t % MIN) / SEC)
-  const milli = showUnder ? `.${pad2(Math.floor((t % SEC) / 10))}` : ''
+  const milli = pad2(Math.floor((t % SEC) / 10))
 
-  if (h > 0) return `${h}:${pad2(m)}:${pad2(s)}` + milli
-  if (m > 0) return `${m}:${pad2(s)}` + milli
-  return `${s}` + milli
+  if (h > 0) return [`${h}:${pad2(m)}:${pad2(s)}`, milli]
+  if (m > 0) return [`${m}:${pad2(s)}`, milli]
+  return [`${s}`, milli]
 }
 
 function Timer({ total }: { total: number }) {
   const sw = useTimer()
   const [timeStr, setTimeStr] = useState<string>('0')
+  const [timeMilliStr, setTimeMilliStr] = useState<string>('00')
 
   useEffect(() => {
-    setTimeStr(timeToStr(sw.floorTime, sw.status === 'pause'))
+    const [timeStr, milliStr] = timeToStr(sw.floorTime)
+
+    setTimeStr(timeStr)
+    setTimeMilliStr('00')
+    if (sw.status === 'pause') {
+      setTimeMilliStr(milliStr)
+    }
   }, [+sw.time, sw.status])
   useEffect(() => {
     sw.setTime(total)
@@ -33,7 +40,9 @@ function Timer({ total }: { total: number }) {
   return (
     <Style>
       <div className="frame">
-        <span className="time">{timeStr}</span>
+        <span className="time">
+          {timeStr}.<span className="time-ms">{timeMilliStr}</span>
+        </span>
         <div className="controls">
           {sw.status === 'init' && (
             <button onClick={() => sw.start()}>Start</button>
@@ -66,7 +75,9 @@ const Style = styled.div`
     font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif, monospace;
     margin: 5%;
     line-height: 1.05em;
-    font-weight: bold;
+  }
+  .time-ms {
+    font-size: calc(100vh / 3 / 2);
   }
   .frame {
     display: grid;
