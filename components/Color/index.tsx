9a7cad6594ab48c560831701612af9c25eec412c
elzup <guild0105@gmail.com>
2021-05-29 21:17:05 +0900

    update color config

--- components/Color/index.tsx
+++ components/Color/index.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react'
+import { SketchPicker } from 'react-color'
+import Router from 'next/router'
 import styled from 'styled-components'
 
+type ColorConfigProps = {
+  onFinish: () => void
+  color: string
+}
+function ColorConfig({ color, onFinish }: ColorConfigProps) {
+  return (
+    <div>
+      <input
+        defaultValue={color}
+        onChange={(e) => {
+          Router.push({ query: { color: e.target.value } })
+        }}
+      />
+      <button onClick={onFinish}>閉じる</button>
+    </div>
+  )
+}
+
 function Color({ color }: { color: string }) {
-  return <Style style={{ background: color }}></Style>
+  const [configMode, setConfigMode] = useState<boolean>(false)
+
+  if (configMode) {
+    return <ColorConfig color={color} onFinish={() => setConfigMode(false)} />
+  }
+
+  return (
+    <Style
+      onClick={() => setConfigMode(true)}
+      style={{ background: color }}
+    ></Style>
+  )
 }
 
 const Style = styled.div`
