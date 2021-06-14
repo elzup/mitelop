3c60ca6829159cb29c41cb0326cceb0652b3ce3d
elzup <guild0105@gmail.com>
2021-06-15 07:41:49 +0900

    to function

--- components/ColorSelectButton.tsx
+++ components/ColorSelectButton.tsx
@@ -1,79 +1,76 @@
 /* eslint-disable no-invalid-this */
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { RGBColor, SketchPicker } from 'react-color'
 import reactCSS from 'reactcss'
-import { SketchPicker } from 'react-color'
 
-class SketchExample extends React.Component {
-  state = {
-    displayColorPicker: false,
-    color: {
-      r: '241',
-      g: '112',
-      b: '19',
-      a: '1',
-    },
-  }
+function SketchExample() {
+  const [visible, setVisible] = useState<boolean>(false)
+  const [color, setColor] = useState<RGBColor>({
+    r: 241,
+    g: 112,
+    b: 19,
+    a: 1,
+  })
 
-  handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker })
-  }
+  useEffect(() => {
+    console.log('DidMount')
+    return () => {
+      console.log('WillUnmount')
+    }
+  }, [])
 
-  handleClose = () => {
-    this.setState({ displayColorPicker: false })
-  }
+  const handleClick = () => setVisible((v) => !v)
+  const handleClose = () => setVisible(false)
 
-  handleChange = (color) => {
-    this.setState({ color: color.rgb })
-  }
-
-  render() {
-    const styles = reactCSS({
-      default: {
-        color: {
-          width: '36px',
-          height: '14px',
-          borderRadius: '2px',
-          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
-        },
-        swatch: {
-          padding: '5px',
-          background: '#fff',
-          borderRadius: '1px',
-          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-          display: 'inline-block',
-          cursor: 'pointer',
-        },
-        popover: {
-          position: 'absolute',
-          zIndex: '2',
-        },
-        cover: {
-          position: 'fixed',
-          top: '0px',
-          right: '0px',
-          bottom: '0px',
-          left: '0px',
-        },
+  const styles = reactCSS({
+    default: {
+      color: {
+        width: '36px',
+        height: '14px',
+        borderRadius: '2px',
+        background: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
       },
-    })
+      swatch: {
+        padding: '5px',
+        background: '#fff',
+        borderRadius: '1px',
+        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+        display: 'inline-block',
+        cursor: 'pointer',
+      },
+      popover: {
+        position: 'absolute',
+        zIndex: 2,
+      },
+      cover: {
+        position: 'fixed',
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        left: '0px',
+      },
+    },
+  } as const)
 
-    return (
-      <div>
-        <div style={styles.swatch} onClick={this.handleClick}>
-          <div style={styles.color} />
-        </div>
-        {this.state.displayColorPicker ? (
-          <div style={styles.popover}>
-            <div style={styles.cover} onClick={this.handleClose} />
-            <SketchPicker
-              color={this.state.color}
-              onChange={this.handleChange}
-            />
-          </div>
-        ) : null}
+  return (
+    <div>
+      <div style={styles.swatch} onClick={handleClick}>
+        <div style={styles.color} />
       </div>
-    )
-  }
+      {visible && (
+        <div style={styles.popover}>
+          <div style={styles.cover} onClick={handleClose} />
+          <SketchPicker
+            color={color}
+            onChange={(res) => {
+              setColor(res.rgb)
+            }}
+          />
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default SketchExample
