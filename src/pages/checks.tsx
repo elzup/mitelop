62fecc68728c1b6404ea79a73120abdba4ce13c7
elzup <guild0105@gmail.com>
2021-07-14 15:42:19 +0900

    fix checks render dynamic

--- src/pages/checks.tsx
+++ src/pages/checks.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from 'next'
-import ChecksTool from '../components/Checks/ChecksTool'
+import dynamic from 'next/dynamic'
 import Layout from '../components/Layout'
+
+const ChecksPage = dynamic(() => import('../components/Checks/ChecksTool'), {
+  ssr: false,
+})
 
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Checks" reset>
-      <ChecksTool />
+      <ChecksPage />
     </Layout>
   )
 }
