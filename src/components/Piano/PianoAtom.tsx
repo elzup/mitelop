e54f43111e30fdc50872fe834509c246b4b4616d
elzup <guild0105@gmail.com>
2021-10-13 23:20:37 +0900

    update keys

--- src/components/Piano/PianoAtom.tsx
+++ src/components/Piano/PianoAtom.tsx
@@ -7,7 +7,7 @@ import { sound } from './sound'
 
 type Props = {}
 
-const isNote = (note: string): note is Note => /[A-F][1-9]/.test(note)
+const isNote = (note: string): note is Note => /[A-G]#?[1-9]/.test(note)
 const SCALE_CHARS = 'ABCDEFG'.split('')
 const CODE_CHARS = '345'.split('')
 
@@ -17,14 +17,42 @@ const keyboardNotes = CODE_CHARS.map((cc) =>
   .flat()
   .filter(isNote)
 
-const noteByKey: Record<string, Note> = {
-  a: 'A3',
-  s: 'B3',
-  d: 'C3',
-  f: 'D3',
-  g: 'E3',
+const octKeysB = 'wetyuio['.split('') // C# D# F# G# A#
+const octKeys = `asdfghjkl;'`.split('') // CDEFGAB
+
+const oct = 3
+const notes = 'CDEFGAB'.split('')
+const notesB = 'C# D# F# G# A#'.split(' ')
+
+const makeNoteMaps = (startOct: number, notes: string[]) => {
+  const noteByKey: Record<string, Note> = {}
+
+  for (const [i, c] of Object.entries(octKeys)) {
+    const oct = startOct + Math.floor(Number(i) / 7)
+    const noteKey = notes[Number(i) % 7]
+    const note = `${noteKey}${oct}`
+
+    if (isNote(note)) {
+      noteByKey[c] = note
+    }
+  }
+
+  for (const [i, c] of Object.entries(octKeysB)) {
+    const oct = startOct + Math.floor(Number(i) / 5)
+    const noteKey = notesB[Number(i) % 5]
+    const note = `${noteKey}${oct}`
+
+    if (isNote(note)) {
+      noteByKey[c] = note
+    }
+  }
+
+  return { noteByKey, keyByNote: swapKeyValue(noteByKey) } as const
 }
-const keyByNote = swapKeyValue(noteByKey)
+
+const { noteByKey, keyByNote } = makeNoteMaps(oct, notes)
+
+console.log({ noteByKey, keyByNote })
 
 function PianoAtom({}: Props) {
   useKeyPressEvent(
