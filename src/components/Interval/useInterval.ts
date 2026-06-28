769dfef4d821de45b5d395544584be14348d663f
elzup <guild0105@gmail.com>
2026-06-29 01:31:00 +0900

    chore: remove unused useEffect import

--- src/components/Interval/useInterval.ts
+++ src/components/Interval/useInterval.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useSeconds } from 'use-seconds'
 import { IntervalStep, IntervalStepBase } from '../../types'
 
