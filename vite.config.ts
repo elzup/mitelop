5397c972959c3ce7562bd5f5e1b490b04bb4c23a
elzup <guild0105@gmail.com>
2026-06-29 10:39:52 +0900

    chore: add vite config and drop static manifest
    vite.config.ts holds the Vite/PWA setup; the static public/manifest.json is now generated by vite-plugin-pwa

---
+++ vite.config.ts
@@ -0,0 +1,64 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            { displayName: true, preprocess: false },
+          ],
+        ],
+      },
+    }),
+    // eslint-disable-next-line new-cap
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['images/icon.png', 'images/icon-4x.png'],
+      manifest: {
+        short_name: 'Mitelop',
+        name: 'Mitelop',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#2B0065',
+        icons: [
+          {
+            src: '/images/icon-4x.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: '/images/icon-512w.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+        protocol_handlers: [
+          {
+            protocol: 'mitelop',
+            url: '/%s',
+          },
+        ],
+        shortcuts: [
+          {
+            name: 'Clock',
+            short_name: 'clock',
+            url: '/clock',
+          },
+          {
+            name: 'Timer',
+            short_name: 'timer',
+            url: '/timer',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+      },
+    }),
+  ],
+})
